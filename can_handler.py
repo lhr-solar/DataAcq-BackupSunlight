6b09478f455e94c16ec4e0597201d915570c731e
Sidharth N. Babu <sidharth.n.babu@gmail.com>
2023-12-09 16:15:24 -0600

    can handler draft 3

--- can_handler.py
+++ can_handler.py
@@ -1,7 +1,12 @@
+'''
+This module handles the ingestion of CAN Messages from the bus,
+formats them, and outputs them to our internal Ethernet message queue.
+The Ethernet module will then send these messages to the host.
+'''
+
 import asyncio
 import can
 from ethernet_handler import ethernet_put
-
 
 # CAN bus object
 bus = can.Bus(
@@ -29,15 +34,20 @@ def _can_msg_callback(msg: can.Message) -> None:
     _can_queue.put_nowait(msg)
 
 
-async def _can_put_in_ethernet_queue() -> None:
+async def can_main() -> None:
     '''
     This function will pull CAN messages from the internal CAN queue, process them, 
     and push to the ethernet queue
     '''
+    # Creating Notifer with an explicit loop to use for scheduling of callbacks
+    # This is necessary because the CAN bus is interrupt driven
+    loop = asyncio.get_running_loop()
+    can.Notifier(bus=bus, listeners=[_can_msg_callback], loop=loop)
+
+    # Process CAN messages as they come into the queue.
     while True:
         # Wait for a CAN message to be put into the queue
         msg = await _can_queue.get()
-
         # Process the CAN message
         header = bytearray([0x03, 0x10])
         msg_id = msg.arbitration_id
@@ -51,19 +61,5 @@ async def _can_put_in_ethernet_queue() -> None:
             data = msg.data.ljust(8, b'\x00')
         msg_id = bytearray(msg_id.to_bytes(4, "little"))
         packet = header + msg_id + idx + data
-
         # Push the packet to the ethernet queue
         ethernet_put(packet)
-
-async def can_main() -> None:
-    '''
-    This is the interrupt function for the CAN bus.
-    This sets up a notifier to trigger the _can_msg_callback function
-    '''
-    # Creating Notifer with an explicit loop to use for scheduling of callbacks
-    # This is necessary because the CAN bus is interrupt driven
-    loop = asyncio.get_running_loop()
-    # notifier = can.Notifier(bus=bus, listeners=[_can_msg_callback], loop=loop)
-    can.Notifier(bus=bus, listeners=[_can_msg_callback], loop=loop)
-    while True:
-        await asyncio.sleep(1)
